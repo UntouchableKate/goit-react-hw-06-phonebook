9e24abdc8592da58dfcdd169c2ff9803ace042e5
Your Name <kateryna.skliaruk6@gmail.com>
2020-01-11 23:03:41 +0100

    Updates

---
+++ precache-manifest.be6777f67328f2aa998e70b05e4746a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9ff63379af2fcbd2ec1fb2d4ef2c5a7",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "50aee15174a93e3830fb",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.0e004a77.chunk.css"
+  },
+  {
+    "revision": "244ebbe9298b7e49c379",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.0cbc67ba.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.0cbc67ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "50aee15174a93e3830fb",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.eca194e7.chunk.js"
+  },
+  {
+    "revision": "3ce19e6931c86c769249",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.0689f783.js"
+  }
+]);
